a15c3fe90c7ba8622315ea0e55e57395f1eb6f3d
Enell261 <66371138+Enell261@users.noreply.github.com>
2023-05-17 09:13:10 +0000

    improved login screen

--- app.py
+++ app.py
@@ -8,21 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 #define a function
 def hello_world():
-  return render_template('home.html')
-
-@app.route('/login', methods=['POST','GET'])
-def login():
-  if request.method=="POST":
-    #Get the submitted m-number
-    m_number = request.form.get('m-number')
-    #Check m-number against approved m-numbers
-    #Add logic here
-    #Redirect user to the next page (UCN page)
-    return redirect(url_for('dashboard'))
   return render_template('login.html')
-@app.route('/dashboard')
-def dashboard():
-  return render_template('dashboard.html')
-    
+
+   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
